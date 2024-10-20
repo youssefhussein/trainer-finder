da5fcf4bf956be3454bd56824c060083ca0f26d9
youssef hussein <20634930+youssefhussein@users.noreply.github.com>
2024-10-20 21:28:03 +0300

    created tabs for home page, added 2 static images

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,5 +1,96 @@
 import { PageProps, User } from "@/types";
 import { Head, Link } from "@inertiajs/react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+function a11yProps(index: number) {
+    return {
+        id: `vertical-tab-${index}`,
+        "aria-controls": `vertical-tabpanel-${index}`,
+    };
+}
+
+  function VerticalTabs() {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+
+    return (
+        <Box
+            sx={{
+                flexGrow: 1,
+                bgcolor: "background.paper",
+                display: "flex",
+                // height: 224, default
+                height: 290,
+            }}
+        >
+            <Tabs
+                orientation="vertical"
+                variant="scrollable"
+                value={value}
+                onChange={handleChange}
+                aria-label="Vertical tabs example"
+                sx={{ borderRight: 1, borderColor: "divider" }}
+            >
+                <Tab label="Item One" {...a11yProps(0)} />
+                <Tab label="Item Two" {...a11yProps(1)} />
+                <Tab label="Item Three" {...a11yProps(2)} />
+                <Tab label="Item Four" {...a11yProps(3)} />
+            </Tabs>
+            <TabPanel value={value} index={0}>
+                <img src="/imgs/dumbell.jpg" alt="img" className="max-w-80 max-h-80" />
+                <h3 className="font-semibold items-center text-orange-500">We are the coolest no cap</h3>
+                <p className="text-slate-800"> some cool af description</p>
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                Item Two
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+                Item Three
+            </TabPanel>
+            <TabPanel value={value} index={3}>
+                Item Four
+            </TabPanel>
+            <TabPanel value={value} index={4}>
+                Item Five
+            </TabPanel>
+        </Box>
+    );
+}
+
 
 function LoginNav(auth: any) {
     return (
@@ -16,16 +107,7 @@ function LoginNav(auth: any) {
     );
 }
 
-function Classes() {
-    return (
-        <section>
-            <header className="font-bold">
-                {" "}
-                Our <span className="text-red-950">classes</span>
-            </header>
-        </section>
-    );
-}
+
 
 export default function Welcome({ auth }: PageProps<{}>) {
     console.log(typeof auth);
@@ -34,7 +116,7 @@ export default function Welcome({ auth }: PageProps<{}>) {
             <Head title="Welcome" />
 
             <LoginNav auth={auth} />
-            <Classes />
+            <VerticalTabs />
         </>
     );
 }
