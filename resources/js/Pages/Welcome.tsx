71846e9a9bdd4914623b5e6f57cdaf19b49ff2d7
youssef hussein <20634930+youssefhussein@users.noreply.github.com>
2024-11-16 22:46:41 +0200

    re-added shadcn ui cuz they fixed the bug, color green,dark mode default

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,28 +1,22 @@
-import VerticalTabs from "@/Components/_welcome/Tab_section";
+import { Button } from "@/Components/ui/button";
 import { PageProps, User } from "@/types";
 import { Head, Link } from "@inertiajs/react";
-import { Button, ButtonGroup } from "@mui/material";
 
-function LoginNav(auth: any) {
+function LoginNav({ auth }:any) {
     return (
         <nav className="relative flex flex-row">
             {auth.user ? (
                 <>
-                    <Button
-                        color="primary"
-                        variant="contained"
-                        className="absolute right-4 top-4"
-                        size="small"
-                    >
+                    <Button color="primary" className="absolute right-4 top-4">
                         <Link href={route("dashboard")}>Dashboard</Link>
                     </Button>
                 </>
             ) : (
                 <div className="absolute right-4 top-4 flex flex-row gap-3">
-                    <Button color="primary" variant="contained" size="small">
+                    <Button color="primary">
                         <Link href={route("login")}>Log in</Link>
                     </Button>
-                    <Button color="primary" variant="outlined" size="small">
+                    <Button color="primary">
                         <Link href={route("register")}>Register</Link>
                     </Button>
                 </div>
@@ -38,8 +32,7 @@ export default function Welcome({ auth }: PageProps<{}>) {
 
             <LoginNav auth={auth} />
             <div className="flex h-1/3 w-full flex-col items-center justify-center">
-                <VerticalTabs />
-                
+helo
             </div>
         </div>
     );
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -22,7 +22,7 @@
     @inertiaHead
 </head>
 
-<body class="font-sans antialiased">
+<body class="font-sans antialiased dark">
     @inertia
 </body>
 
